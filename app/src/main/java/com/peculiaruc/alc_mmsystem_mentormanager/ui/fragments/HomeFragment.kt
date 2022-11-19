65c2aa917b5f32024e552bb6e961a57a412b244d
kevNjunge <njungechuguka@gmail.com>
2022-11-19 22:25:59 +0300

    mentor manager dashboard ui

--- app/src/main/java/com/peculiaruc/alc_mmsystem_mentormanager/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/peculiaruc/alc_mmsystem_mentormanager/ui/fragments/HomeFragment.kt
@@ -7,14 +7,6 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.peculiaruc.alc_mmsystem_mentormanager.databinding.FragmentHomeBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
     lateinit var binding: FragmentHomeBinding
 
@@ -22,6 +14,8 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+
         // Inflate the layout for this fragment
         binding = FragmentHomeBinding.inflate(inflater)
         return binding.root
