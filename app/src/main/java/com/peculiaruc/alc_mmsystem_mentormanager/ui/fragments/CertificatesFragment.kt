68f5c92e87acbb91005d503b96db7e4e041f69ae
HP <robsonidongesitsamuel@gmail.com>
2022-11-20 09:09:21 +0100

    Certificate dashboard

--- app/src/main/java/com/peculiaruc/alc_mmsystem_mentormanager/ui/fragments/CertificatesFragment.kt
+++ app/src/main/java/com/peculiaruc/alc_mmsystem_mentormanager/ui/fragments/CertificatesFragment.kt
@@ -5,26 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentTransaction
+import androidx.navigation.findNavController
 import com.peculiaruc.alc_mmsystem_mentormanager.R
+import com.peculiaruc.alc_mmsystem_mentormanager.databinding.FragmentCertificatesBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [CertificatesFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CertificatesFragment : Fragment() {
+    lateinit var binding: FragmentCertificatesBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_certificates, container, false)
+        binding = FragmentCertificatesBinding.inflate(inflater)
+
+        binding.certBtn.setOnClickListener {
+            val action = CertificatesFragmentDirections.actionCertificatesFragmentToApprovedCertificateFragment()
+            it.findNavController().navigate(action)
+        }
+        return binding.root
     }
 
 }
