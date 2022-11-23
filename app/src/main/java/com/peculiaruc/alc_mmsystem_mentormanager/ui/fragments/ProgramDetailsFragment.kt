8063f65b973176ada4291df5c39294ca2b15d5a9
HP <robsonidongesitsamuel@gmail.com>
2022-11-23 12:50:32 +0100

    Program Details and cert request_dialog

---
+++ app/src/main/java/com/peculiaruc/alc_mmsystem_mentormanager/ui/fragments/ProgramDetailsFragment.kt
@@ -0,0 +1,43 @@
+package com.peculiaruc.alc_mmsystem_mentormanager.ui.fragments
+
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import com.peculiaruc.alc_mmsystem_mentormanager.R
+import com.peculiaruc.alc_mmsystem_mentormanager.databinding.FragmentProgramDetailsBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+
+class ProgramDetailsFragment : Fragment() {
+    lateinit var binding: FragmentProgramDetailsBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentProgramDetailsBinding.inflate(inflater)
+
+        binding.applyBtn.setOnClickListener {
+            val dialogBinding = layoutInflater.inflate(R.layout.program_dialog, null)
+            val myDialog = Dialog(requireContext())
+            myDialog.setContentView(dialogBinding)
+            myDialog.setCancelable(true)
+            myDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+            val dialogDone = dialogBinding.findViewById<Button>(R.id.cert_request_done)
+            dialogDone.setOnClickListener {
+                myDialog.dismiss()
+            }
+            myDialog.dismiss()
+        }
+        return binding.root
+    }
+}
